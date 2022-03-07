bab4f74f0c5ad3830d1f374ca42379c55ca2e425
LukaJoksimovic <luka.joksimovic04@gmail.com>
2022-03-07 22:10:52 +0100

    Set up default page

---
+++ sitemap.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <table>
+        <tr>
+            <th><a href="">Hauptseite</a></th>
+            <th></th>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td><a href="">Über uns</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Geschäftsleitung</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Werden Sie Partner</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Rückgaberichtlinien</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Geschäftsleitung</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Allgemeine Geschäftsbedingungen (AGB)</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Impressum</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Geschäftsleitung</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Spenden Sie jetzt</a></td>
+        </tr>
+
+
+        <tr>
+            <td></td>
+            <td><a href="">Produkte</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Kategorie</a></td>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Produktdetails</a></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Produkte vergleichen</a></td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td><a href="">Account</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Zahlungsarten</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Passwort ändern</a></t>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Gespeicherte Addressen</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Bestellungen</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Geschenkkarten</a></td>
+        </tr>
+
+
+
+        <tr>
+            <td></td>
+            <td><a href="">Einkaufswagen</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Zusammenfassung Bestellung</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Zahlungsart</a></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td><a href="">Versandart</a></td>
+        </tr>
+
+    </table>
+</body>
+</html>
